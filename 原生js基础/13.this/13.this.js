8b19c22c7aa4aa1dcde6d77fa418d04a7d1d4c3e
qiaoshi <qiaoshi@chuchujie.com>
2019-04-08 00:09:53 +0800

    test

--- 原生js基础/13.this/13.this.js
+++ 原生js基础/13.this/13.this.js
@@ -8,3 +8,99 @@
 //6.obj.fn(),函数执行.前面是谁，this就是谁
 //7.箭头函数中的this，是函数声明时就确定的，指的是函数声明时所处的上下文。
 
+
+
+// console.log('1');
+// setTimeout(function() {
+//     console.log('2');
+//     new Promise(function(resolve) {
+//         console.log('4');
+//         resolve();
+//     }).then(function() {
+//         console.log('5'); setTimeout(function(){console.log(100)})
+//     })
+// },500)
+//
+// setTimeout(function() {
+//     console.log('6');
+//     new Promise(function(resolve) {
+//         console.log('7');
+//         resolve();
+//     }).then(function() {
+//         console.log('8')
+//     })
+// },500)
+// 1,2,4,6,7,5,8
+
+// console.log(1)
+// setTimeout(function () {
+//     console.log(2);
+//     setImmediate(function () {
+//         console.log(5)
+//     })
+//     process.nextTick(function () {
+//         console.log(6)
+//     })
+//     new Promise((resolve,reject)=>{
+//         console.log(7);
+//         resolve()
+//     }).then(function () {
+//         console.log(8)
+//     })
+// },100);
+// process.nextTick(function () {
+//     console.log(3)
+// });
+// setImmediate(function () {
+//     console.log(4)
+// });
+
+// 1,3,4,2,7,6,8,5
+
+
+console.log('--------------')
+
+console.log('1');
+setTimeout(function() {
+    console.log('2');
+    new Promise(function(resolve) {
+        console.log('4');
+        resolve();
+    }).then(function() {
+        console.log('5');
+        setTimeout(function(){console.log(11)})
+    })
+    process.nextTick(function () {
+        console.log(10)
+    })
+    setImmediate(function () {
+        console.log(12)
+    })
+},500);
+
+setTimeout(function() {
+    console.log('6');
+    new Promise(function(resolve) {
+        console.log('7');
+        resolve();
+    }).then(function() {
+        console.log('8')
+    })
+    process.nextTick(function () {
+        console.log(9)
+    })
+},500)
+
+// 第一次事件环
+//timer 阶段 1，2，4，6，7，; nexttick:10,9;//微任务队列  5，8
+//io 阶段 无
+//check 阶段 12
+//close 阶段 无
+
+//第二次事件环
+//timer 阶段 11;
+//io 阶段 无
+//check 阶段
+//close 阶段
+
+
